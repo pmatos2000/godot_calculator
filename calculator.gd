103a9c6faf3c0baab6af886cdf5582a8b522536b
Paulo <pmatos2000@gmail.com>
2024-05-09 15:23:07 -0300

    Metodo add ponto

--- calculator.gd
+++ calculator.gd
@@ -4,17 +4,16 @@ const NAME_GROUP_BUTTON := "button"
 const NAME_GROUP_DISPLAY := "display"
 const NAME_SIGNAL_BUTTON_PRESSED := "pressed"
 const NAME_FUNC_BUTTON_PRESSED := "_button_pressed"
-const NUMBER_ZERO := "0"
 
 var display: Label
 
 enum IdNumber { First, Second}
 var id_current_number := IdNumber.First
 
-var list_number: Array[String] = [NUMBER_ZERO, NUMBER_ZERO]
+var list_number: Array[String] = ["0", "0"]
 
 func _add_digit(digit: String) -> void:
-	if(list_number[id_current_number] == NUMBER_ZERO):
+	if(list_number[id_current_number] == "0"):
 		list_number[id_current_number] = digit
 	else:
 		list_number[id_current_number] = list_number[id_current_number] + digit
@@ -22,8 +21,12 @@ func _add_digit(digit: String) -> void:
 func _delete_digit() -> void:
 	list_number[id_current_number] = list_number[id_current_number].left(-1)
 	if(list_number[id_current_number].is_empty()):
-		list_number[id_current_number] = NUMBER_ZERO
+		list_number[id_current_number] = "0"
 
+func _add_point() -> void:
+	if !list_number[id_current_number].contains("."):
+		list_number[id_current_number] = list_number[id_current_number] + "."
+		
 
 func _button_pressed(button_name: String) -> void:
 	match button_name:
@@ -31,7 +34,9 @@ func _button_pressed(button_name: String) -> void:
 			_add_digit(button_name)
 		"Backspace":
 			_delete_digit()
-			
+		"Ponto":
+			_add_point()
+
 func _ready() -> void:
 	display = get_tree().get_first_node_in_group(NAME_GROUP_DISPLAY)
 	for button in get_tree().get_nodes_in_group(NAME_GROUP_BUTTON):
